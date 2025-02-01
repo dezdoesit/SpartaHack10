7459c797d763eaa6774c9a6830773c13b6d01e9d
dezdoesit <dez.blair06@gmail.com>
2025-02-01 15:27:30 -0500

    Init VisionPro Proj

---
+++ InterView/InterView/InterViewApp.swift
@@ -0,0 +1,33 @@
+//
+//  InterViewApp.swift
+//  InterView
+//
+//  Created by Dezmond Blair on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct InterViewApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
